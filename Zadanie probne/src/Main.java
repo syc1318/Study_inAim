acabfae9a341b79b9d4e9a4819824b7e8a4c468c
syc1318 <ye_chan_seo@naver.com>
2022-08-16 10:15:19 +0900

    SomeBV

--- Zadanie probne/src/Main.java
+++ Zadanie probne/src/Main.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        char[] c = s.toCharArray();
+        for(int i = c.length - 1; i >= 0; i--) {
+            System.out.print(c[i]);
+        }
     }
 }
