2d14fb1345451c50fefa07e084a659dc3e38c9ee
Yechan Seo <ycseo@aim.co.kr>
2024-01-25 17:24:50 +0900

    test

--- Back2845/src/Main.java
+++ Back2845/src/Main.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
+//test
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int l = scanner.nextInt();
         int p = scanner.nextInt();
-        int pp =l*p;
-        for(int i = 0; i < 5; i++) {
+        int pp = l * p;
+        for (int i = 0; i < 5; i++) {
             int a = scanner.nextInt();
-            System.out.println(a-pp);
+            System.out.println(a - pp);
         }
     }
 }
